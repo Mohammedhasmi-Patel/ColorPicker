e6de336a54741bc487b20196cbf4811df4c5909d
Mohammedhasmi-Patel <145280415+Mohammedhasmi-Patel@users.noreply.github.com>
2024-01-28 13:17:42 +0530

    Add files via upload
    here i have written all the logic of javascript that how actually work

---
+++ ColorPro.js
@@ -0,0 +1,61 @@
+let entireBackground = document.querySelector("body");
+let redButton = document.getElementById("red");
+let aquaButton = document.getElementById("aqua");
+let aquamarineButton = document.getElementById("aquamarine");
+let blueButton = document.getElementById("blue");
+let yellowgreenButton = document.getElementById("yellowgreen");
+let greenButton = document.getElementById("green");
+let yellowButton = document.getElementById("yellow");
+let goldButton = document.getElementById("gold");
+let silverButton = document.getElementById("silver");
+let orangeButton = document.getElementById("orange");
+
+/* this is for red background color  */
+redButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+});
+
+/* this is for aqua background color  */
+aquaButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "aqua";
+});
+
+/* this is for aquamarine background color */
+aquamarineButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "aquamarine";
+});
+
+/* this background is for blue color  */
+blueButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+
+/* this background is for yellowGreen color  */
+yellowgreenButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellowgreen";
+});
+
+/* this background is for Green color  */
+greenButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+});
+
+/* this background is for yellow color  */
+yellowButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellow";
+});
+
+/* this background is for gold color  */
+goldButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "gold";
+});
+
+/* this background is for silvet color  */
+silverButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "silver";
+});
+
+/* this background is for orange color  */
+orangeButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "orange";
+});
